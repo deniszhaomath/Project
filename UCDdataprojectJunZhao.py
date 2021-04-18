2453f24a302a9c31b4e733ef41ff01c31cc2c3cd
Jun Zhao <zhaoju@tcd.ie>
2021-04-18 16:10:24 +0100

    imported csv file

--- UCDdataprojectJunZhao.py
+++ UCDdataprojectJunZhao.py
@@ -5,4 +5,9 @@ Created on Mon Apr 12 23:02:05 2021
 @author: Home
 """
 
-kaggle datasets download -d ajaypalsinghlo/world-happiness-report-2021
+#kaggle datasets download -d ajaypalsinghlo/world-happiness-report-2021
+import pandas as pd
+world_happiness_report_2021 = pd.read_csv("C:\\Users\\Home\\Desktop\\UCD Professional Academy\\world-happiness-report-2021.csv")
+print (world_happiness_report_2021.head())
+print (world_happiness_report_2021.info())
+print (world_happiness_report_2021.isna().any())
