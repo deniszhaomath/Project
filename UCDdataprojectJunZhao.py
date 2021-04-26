09f56c6f9fb982bccda91fec969defa76c4a646b
Jun Zhao <zhaoju@tcd.ie>
2021-04-26 14:33:03 +0100

    made second change

--- UCDdataprojectJunZhao.py
+++ UCDdataprojectJunZhao.py
@@ -11,3 +11,32 @@ world_happiness_report_2021 = pd.read_csv("C:\\Users\\Home\\Desktop\\UCD Profess
 print (world_happiness_report_2021.head())
 print (world_happiness_report_2021.info())
 print (world_happiness_report_2021.isna().any())
+print (world_happiness_report_2021[:4])
+
+australia_rain = pd.read_csv("C:\\Users\\Home\\Desktop\\UCD Professional Academy\\datasets\\Australia rain\\weatherAUS.csv")
+print (australia_rain)
+print (australia_rain.head())
+print (australia_rain.isna().any())
+#australia_rain_index = pd.DataFrame(index = "location")
+
+#import API request
+import requests
+print (requests.get('http://google.com'))
+
+
+#API for open-notify.org
+iss = requests.get('http://api.open-notify.org/iss-now.json')
+print (iss.text)
+issj = iss.json()
+print (issj)
+print (issj['iss_position'])
+
+people=requests.get('http://api.open-notify.org/astros.json')
+print (people.json()['people'])
+
+#print people name in a for loop
+i = 1
+for name in people.json()['people']:
+    print (str(i) + ' ', name['name'])
+    i = i + 1
+    
